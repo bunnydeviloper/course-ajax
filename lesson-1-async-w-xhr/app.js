199ca13ce33b73089142cb6b441f997589652d0a
Sophia Mai <phuongmai001@gmail.com>
2018-08-13 15:17:07 -0700

    update ajax request

--- lesson-1-async-w-xhr/app.js
+++ lesson-1-async-w-xhr/app.js
@@ -8,35 +8,34 @@
     e.preventDefault();
     responseContainer.innerHTML = '';
     searchedForText = searchField.value;
-  });
 
-  function requestError(err) {
-    console.log(err);
-  }
+    function requestError(err) {
+      console.log(err);
+    }
 
-  function addImage() {
-    let htmlContent = '';
-    const data = JSON.parse(this.responseText);
-    if (data && data.results && data.results[0]) {
-      const firstImage = data.results[0];
-      htmlContent = `<figure>
+    function addImage() {
+      let htmlContent = '';
+      const data = JSON.parse(this.responseText);
+      if (data && data.results && data.results[0]) {
+        const firstImage = data.results[0];
+        htmlContent = `<figure>
         <img src="${firstImage.urls.regular}" alt="${searchedForText}">
         <figcaption>${searchedForText} by ${firstImage.user.name}</figcaption>
         </figure>`;
-    } else {
-      htmlContent = '<div class="error-no-image">No images available</div>';
+      } else {
+        htmlContent = '<div class="error-no-image">No images available</div>';
+      }
+      responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
     }
-  responseContainer.insertAdjacentHTML('afterbegin', htmlContent);
-  }
 
-  const unsplashRequest = new XMLHttpRequest();
-  unsplashRequest.onload = addImage;
-  unsplashRequest.onerror = function (err) {
-    requestError(err, 'image');
-  };
-  searchedForText = 'hippos';
+    const unsplashRequest = new XMLHttpRequest();
+    unsplashRequest.onload = addImage;
+    unsplashRequest.onerror = function (err) {
+      requestError(err, 'image');
+    };
 
-  unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
-  unsplashRequest.setRequestHeader('Authorization', 'Client-ID 14a2748ef2fbdb3402b3a43c75c1da7d013c03955ae474f62e3018208fcb01db');
-  unsplashRequest.send();
+    unsplashRequest.open('GET', `https://api.unsplash.com/search/photos?page=1&query=${searchedForText}`);
+    unsplashRequest.setRequestHeader('Authorization', 'Client-ID 14a2748ef2fbdb3402b3a43c75c1da7d013c03955ae474f62e3018208fcb01db');
+    unsplashRequest.send();
+  });
 })();
